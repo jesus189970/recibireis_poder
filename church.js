18d0f1fedd79560dac1d843fb1467f43a071768a
jesus189970 <68864495+jesus189970@users.noreply.github.com>
2022-05-12 17:55:30 -0400

    Add files via upload

--- church.js
+++ church.js
@@ -14,7 +14,7 @@ const prevBtn = document.querySelector('#prevBtn');
 const nextBtn = document.querySelector('#nextBtn');
 
 //Counter
-let counter = 1;
+let counter = 0;
 const size = carouselImages[0].clientWidth;
 
 carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
