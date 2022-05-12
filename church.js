5c704e18638c21c39c85df3c678d712c6a279e47
jesus189970 <68864495+jesus189970@users.noreply.github.com>
2022-05-12 17:44:25 -0400

    Add files via upload

--- church.js
+++ church.js
@@ -5,3 +5,46 @@ menu_btn.addEventListener('click', function () {
     menu_btn.classList.toggle('is-active');
 		mobile_nav.classList.toggle('is-active');
 });
+
+const carouselSlide = document.querySelector('.carousel-slide');
+const carouselImages = document.querySelectorAll('.carousel-slide img');
+
+//Buttons
+const prevBtn = document.querySelector('#prevBtn');
+const nextBtn = document.querySelector('#nextBtn');
+
+//Counter
+let counter = 1;
+const size = carouselImages[0].clientWidth;
+
+carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+
+//Nutton Listeners
+nextBtn.addEventListener('click',function(){
+    if (counter >= carouselImages.length -1) return;
+    carouselSlide.style.transition = "transform 0.4s ease-in-out"
+    counter++;
+    carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+});
+
+prevBtn.addEventListener('click',function(){
+    if (counter <= 0) return;
+    carouselSlide.style.transition = "transform 0.4s ease-in-out"
+    counter--;
+    carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+});
+
+carouselSlide.addEventListener('transitionend', function(){
+    if (carouselImages[counter].id === 'lastClone') {
+        carouselSlide.style.transition = 'none'
+        counter = carouselImages.length -2 ;
+        carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+    }
+
+    if (carouselImages[counter].id === 'firstClone') {
+        carouselSlide.style.transition = 'none'
+        counter = carouselImages.length - counter ;
+        carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+    }
+
+})
